057ec02b64fc210752ac1ef95a6b139e26fab850
longyanghe <742781499@qq.com>
2021-12-07 17:23:12 +0800

    add new function

--- app/src/main/java/com/example/mymusicplayer/ui/music/MusicFragment.kt
+++ app/src/main/java/com/example/mymusicplayer/ui/music/MusicFragment.kt
@@ -1,27 +1,53 @@
 package com.example.mymusicplayer.ui.music
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Lifecycle
+import com.blankj.utilcode.util.Utils
 import com.example.mymusicplayer.BaseFragment
 import com.example.mymusicplayer.R
-import com.example.mymusicplayer.databing.MusicBinding
+import com.example.mymusicplayer.databinding.MusicFragmentBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
 //音乐页面
-class MusicFragment : BaseFragment<MusicBinding,MusicViewModel>() {
+class MusicFragment : BaseFragment<MusicFragmentBinding,MusicViewModel>() {
 
     companion object {
         fun newInstance() = MusicFragment()
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mDataBinding.message.setOnClickListener {
+            mViewModel.changeData("data")
+        }
+        GlobalScope.launch (Dispatchers.Main){
+            mViewModel.stateFlow.collect {
+                mDataBinding.message.text = it
+            }
+        }
+    }
     override fun getContentViewId(): Int {
         return R.layout.music_fragment
     }
 
     override fun bindViewModel() {
-
+        mDataBinding.viewmodel = mViewModel
     }
 
     override fun getViewModelClass(): Class<MusicViewModel> {
         return MusicViewModel::class.java
     }
-
 
 }
