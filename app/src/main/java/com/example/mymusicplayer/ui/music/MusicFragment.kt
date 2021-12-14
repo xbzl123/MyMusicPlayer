b11a4f9d4dc6ccb02ddb9cfaf49efe722ae09766
longyanghe <742781499@qq.com>
2021-12-14 16:55:14 +0800

    增加获取本地音乐后面播放

--- app/src/main/java/com/example/mymusicplayer/ui/music/MusicFragment.kt
+++ app/src/main/java/com/example/mymusicplayer/ui/music/MusicFragment.kt
@@ -1,6 +1,12 @@
 package com.example.mymusicplayer.ui.music
 
+import android.media.AudioManager
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Environment
+import android.os.FileUtils
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -23,20 +29,67 @@ class MusicFragment : BaseFragment<MusicFragmentBinding,MusicViewModel>() {
         fun newInstance() = MusicFragment()
     }
 
+    var musicInfoList = arrayListOf<MusicInfo>()
     override fun onResume() {
         super.onResume()
-        mDataBinding.message.setOnClickListener {
+        searchMp3File()
+
+        val function: (View) -> Unit = {
+            if(mViewModel.isPlay.get()){
+            playMusic(musicInfoList[0].path)
+            }
 //            mViewModel.changeData("data")
         }
+        mDataBinding.message.setOnClickListener(function)
         GlobalScope.launch (Dispatchers.Main){
             mViewModel.stateFlow.collect {
                 mDataBinding.message.text = it
             }
         }
     }
+
+    private fun searchMp3File() {
+        val query = context?.contentResolver?.query(
+            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+            null,
+            null,
+            null,
+            MediaStore.Audio.Media.DEFAULT_SORT_ORDER
+        )
+        if (query != null){
+            while (query.moveToNext()){
+                var musicInfo = MusicInfo(query.getLong(query.getColumnIndexOrThrow(MediaStore.Audio.Media._ID))
+                    ,query.getString(query.getColumnIndexOrThrow(MediaStore.Audio.Media.DISPLAY_NAME))
+                    ,query.getString(query.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST))
+                    ,query.getInt(query.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION))
+                    ,query.getLong(query.getColumnIndexOrThrow(MediaStore.Audio.Media.SIZE))
+                    ,query.getString(query.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA)))
+                musicInfoList.add(musicInfo)
+            }
+            Log.d("1111", "ssssize = "+musicInfoList[0].path)
+        }
+
+    }
+
+    private fun playMusic(path: String) {
+        val mediaPlayer = MediaPlayer().apply {
+            setAudioStreamType(AudioManager.STREAM_MUSIC)
+            setDataSource(path)
+            //异步监听加载
+            prepareAsync()
+        }
+
+        mediaPlayer.setOnPreparedListener({
+            it.start()
+            mViewModel.isPlay.set(true)
+        })
+    }
+
     override fun getContentViewId(): Int {
         return R.layout.music_fragment
     }
+
+
 
     override fun bindViewModel() {
         mDataBinding.viewmodel = mViewModel
--- app/src/main/java/com/example/mymusicplayer/ui/music/MusicViewModel.kt
+++ app/src/main/java/com/example/mymusicplayer/ui/music/MusicViewModel.kt
@@ -3,17 +3,19 @@ package com.example.mymusicplayer.ui.music
 import android.util.Log
 import android.widget.Toast
 import androidx.databinding.Observable
+import androidx.databinding.ObservableBoolean
 import androidx.databinding.ObservableField
 import com.blankj.utilcode.util.Utils
 import com.example.mymusicplayer.viewmodel.LifecycleViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
-data class MusicInfo(val id:Int,val songName:String,val songer:String,val timese:String)
+data class MusicInfo(var id:Long, var songName:String, var songer:String, var duration:Int, var size:Long,var path:String)
 
 class MusicViewModel : LifecycleViewModel() {
 
     var nameObserver: ObservableField<String> = ObservableField("test")
+    var isPlay:ObservableBoolean = ObservableBoolean(true)
     private val temp = MutableStateFlow("test over!")
     val stateFlow:StateFlow<String> = temp
     fun changeData(content:String){
