83d3e1119b26749ad15f4d818761b92c1ad1f4f7
longyanghe <742781499@qq.com>
2021-12-15 17:50:54 +0800

    增加音乐界面图片加载

--- app/src/main/java/com/example/mymusicplayer/ui/music/MusicViewModel.kt
+++ app/src/main/java/com/example/mymusicplayer/ui/music/MusicViewModel.kt
@@ -1,9 +1,15 @@
 package com.example.mymusicplayer.ui.music
 
+import android.graphics.BitmapFactory
 import android.media.AudioManager
 import android.media.MediaPlayer
+import android.os.Build
+import android.os.Environment
 import android.util.Log
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.databinding.BindingAdapter
 import androidx.databinding.Observable
 import androidx.databinding.ObservableBoolean
 import androidx.databinding.ObservableField
@@ -11,13 +17,23 @@ import com.blankj.utilcode.util.Utils
 import com.example.mymusicplayer.viewmodel.LifecycleViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import java.io.File
 
 data class MusicInfo(var id:Long, var songName:String, var songer:String, var duration:Int, var size:Long,var path:String)
+
+@BindingAdapter(value = ["loadpic"],requireAll = false)
+fun loadMusicAlbumPic(view:ImageView,path: String){
+    view.setImageBitmap(BitmapFactory.decodeFile(path))
+}
 
 class MusicViewModel : LifecycleViewModel() {
 
     var nameObserver: ObservableField<String> = ObservableField("test")
     var isStop:ObservableBoolean = ObservableBoolean(true)
+    var path = ""
+    @RequiresApi(Build.VERSION_CODES.R)
+    var picPath: ObservableField<String> = ObservableField(Environment.getExternalStorageDirectory().absolutePath+"/test1.jpg")
+
     var musicInfoList = arrayListOf<MusicInfo>()
 
     private val temp = MutableStateFlow("test over!")
@@ -25,16 +41,21 @@ class MusicViewModel : LifecycleViewModel() {
     fun changeData(content:String){
         temp.value = content
     }
-
-    fun playMusic(){
-        val mediaPlayer = MediaPlayer().apply {
-            setAudioStreamType(AudioManager.STREAM_MUSIC)
-            setDataSource(musicInfoList.get(0).path)
-            //异步监听加载
-            prepareAsync()
-        }
-
-        if (isStop.get()){
+    var mediaPlayer:MediaPlayer =  MediaPlayer()
+    fun playMusic(path: String){
+        if (!mediaPlayer.isPlaying){
+            if(!path.equals("")){
+                mediaPlayer.start()
+                isStop.set(false)
+                return
+            }
+//            path = musicInfoList.get(0).path
+            mediaPlayer.apply {
+                setAudioStreamType(AudioManager.STREAM_MUSIC)
+                setDataSource(path)
+                //异步监听加载
+                prepareAsync()
+            }
             mediaPlayer.setOnPreparedListener({
                 mediaPlayer.start()
                 isStop.set(false)
@@ -42,6 +63,10 @@ class MusicViewModel : LifecycleViewModel() {
         }else{
             mediaPlayer.pause()
             isStop.set(true)
+
         }
     }
+
+
 }
+
